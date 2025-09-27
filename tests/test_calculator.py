1263e94ae1a333949b3d6db8537a68a3104dc9f3
avizuetem <avizuetem@unemi.edu.ec>
2025-09-27 10:59:06 -0500

    quinto envio de pruebas

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,10 +3,10 @@ from src.calculator import suma, resta
 
 class TestCalculator(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(2, 3), 15)
+        self.assertEqual(suma(2, 3), 5)
     
     def test_resta(self):
-        self.assertEqual(resta(5, 3), 12)
+        self.assertEqual(resta(5, 3), 2)
 
 if __name__ == '__main__':
     unittest.main()
